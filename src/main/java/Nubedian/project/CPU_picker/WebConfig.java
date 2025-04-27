c7158b1186219f0473d8e609c134284390021a50
golololologol <golololologol@gmail.com>
2025-04-28 02:51:50 +0300

    No, revert that

--- src/main/java/Nubedian/project/CPU_picker/WebConfig.java
+++ src/main/java/Nubedian/project/CPU_picker/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://cpu-picker-frontend-production.up.railway.app")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
